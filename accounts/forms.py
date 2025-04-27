0670d1b90eaaa7b5dd44b1c6131fb2f7f2256deb
ehsan44444 <ehsantahafchi@gmail.com>
2025-04-28 00:21:40 +0330

    add customuser model

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.forms import get_user_model
+from django.contrib.auth.admin import AdminUserCreationForm
 
 
-class CustomUserCreationForm(UserCreationForm):
+class CustomUserCreationForm(AdminUserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ('email', 'username')
